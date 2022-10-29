5694433a7bd63af8347abca62944376890b2291f
CygnusDownfall <phapsuhacam112@gmail.com>
2022-10-29 10:23:23 +0700

    vvvv

---
+++ Do_an_CNPM_nhom4/DTO/CSDL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace _020101125.DTO
+{
+    public static class CSDL
+    {
+        static SqlConnection connection;
+        public static void Connect()
+        {
+            string con = @"Data Source=DOWNFALL;Initial Catalog=QLNV28_09;Integrated Security=True";
+            connection = new SqlConnection(con);
+
+            if(connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        public static DataTable Query(string sqlquery)
+        {
+            Connect();
+            DataTable tb = new DataTable();
+            SqlCommand cm = new SqlCommand(sqlquery, connection);
+            tb.Load( cm.ExecuteReader());
+            return tb;
+        }
+        public static void exxcute(string command)
+        {
+            SqlCommand cm=new SqlCommand(command, connection);
+            cm.ExecuteNonQuery();
+
+        }
+    }
+}
